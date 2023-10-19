de92abd666af9cad8ab98a250c0b5bd48b02b02a
itDarashukD <itdarashukd@gmail.com>
2023-10-19 21:00:37 +0200

    add file

--- src/main/java/com/example/rest/RestApplication.java
+++ src/main/java/com/example/rest/RestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"com.example.rest","com.example.restmodule","com.example.api","com.example.dto","com.example.cloud"})
 public class RestApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 	}
